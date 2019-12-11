de6f8bd8365a5015453fc7d9ece8435c6c224055
alonzo0812 <dnobilis@yahoo.com>
2019-12-11 10:50:45 +0800

    xd

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let playerHealth = 0;
 
 function startGame() {
   inventory = {}
-  playerHealth = Math.floor((Math.random()*20 + 100))
+  playerHealth = 100//Math.floor((Math.random()*20 + 100))
   showTextNode(1)
 }
 
@@ -75,7 +75,7 @@ function deleteItem(prop){
 const textNodes = [
   {
     id: 1,
-    text: 'Welcome to "Vengeance of the Sun"! I hope you\'ll enjoy playing this game ;P',
+    text: 'Welcome to "Vengeance of the Sun"! The story of a knight named Curlone, set in the medieval era. I hope you\'ll enjoy playing this game ;P',
     options: [
       {
         text: 'Start the Adventure!',
@@ -83,20 +83,36 @@ const textNodes = [
       },
       {
         text: 'Start the Adventure!',
-        nextText: 2
+        nextText: 2,
       },
     ]
   },
   {
     id: 2,
-    text: `asdfasdfasdfadfasfasdfasdfasdfasdfa`,
+    text: `In the main lands of Schole lies the Kingdom of Destice. A kingdom filled with horrid men reeking of carnal desires and the stench of corruption.
+            Though Destice is a place that rivals hell, some still harbour goodwill in their heart. A cliche isn't it? Now one may ask, in a place where one must be good or bad, does there lie hope for an outcast to exist? A man whose only shade is gray? 
+Surprisingly, there is. And that is you, the mighty knight that balances the opposing forces that eternally clash in the everyday of Destice, the knight named Curlone.
+  And you are standing now atop the castle tower looking below at the wide scenery of madness in front of you, Destice. Knowing that the King wishes to meet you soon for a Quest, you then wore your armor and prepare the best weapon you can yield….
+`,
     options: [{
-      text: 'Continue',
-      nextText: 3///Change
+      text: 'a Sword',
+      nextText: 3,
+      setInventory: {Sword: true, Swordsman: true}
     },
     {
-      text: 'Continue',
-      nextText: 3///Change
+      text: 'a Bow',
+      nextText: 4,
+      setInventory: {Bow: true, Archer: true}
+    },
+    {
+      text: 'Twin Daggers',
+      nextText: 5,
+      setInventory: {Daggers: true, Assassin: true}
+    },
+    {
+      text: 'a Spear',
+      nextText: 6,
+      setInventory: {Spear: true, Spearman: true}
     },
     ]
   },
